acd3740f7ea73886243e736597d35310546b8b5b
Ethan <122648917+lee3291@users.noreply.github.com>
2023-06-03 09:02:29 +0900

    chess piece fields

--- src/Piece.java
+++ src/Piece.java
@@ -1,7 +1,9 @@
 public abstract class Piece {
-    private String name;
-    private String color;
+    private String name; // E4P, D4P, B1K... each unique position and name. P for Pawn, K for Knight.
+    private String type; // pawn, knight...
+    private String color; // Black or White
     private String currentPosition;
 
     // Available move logic?
+    // 2D Array?
 }
