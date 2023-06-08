7e47745f439361f7b2fb5146cc9e461c95e91ffb
Ethan <122648917+lee3291@users.noreply.github.com>
2023-06-08 16:30:52 +0900

    add Board and King

---
+++ src/Board.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Board {
+    // Hashmap of board? key will be coordinates, value will be pieces.
+    private HashMap<String, Piece> board;
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
--- src/Piece.java
+++ src/Piece.java
@@ -4,6 +4,42 @@ public abstract class Piece {
     private String color; // Black or White
     private String currentPosition;
 
-    // Available move logic?
-    // 2D Array?
+    public Piece(String name, String type, String color, String currentPosition) {
+        this.name = name;
+        this.type = type;
+        this.color = color;
+        this.currentPosition = currentPosition;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getCurrentPosition() {
+        return currentPosition;
+    }
+
+    public void setCurrentPosition(String currentPosition) {
+        this.currentPosition = currentPosition;
+    }
 }
